4038b05c7c3c6cccf4a9919e9a361f4595c51433
JNotelddim <jaredmiddleton3.14@gmail.com>
2019-11-01 13:11:43 -0700

    Cycle through available values on cell click event
    right now 3 values are just hard-coded, but later they'll come from
    some defaults (blank, food) plus the dynamic snakes

--- src/comp/Cell.tsx
+++ src/comp/Cell.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {ICell} from '../config/typings.config'
 
+const CELL_VALUES = ["", "x", "f"]
+
 export default class Cell extends Component<ICell>  {
     key: number
     value: any
@@ -11,7 +13,15 @@ export default class Cell extends Component<ICell>  {
         this.value = props.value;
     }
 
+    onClick(e: React.MouseEvent){
+        let currentValueIndex = CELL_VALUES.indexOf(this.value);
+        let newValueIndex = currentValueIndex >= CELL_VALUES.length ? 0 : currentValueIndex + 1
+        let newValue = CELL_VALUES[newValueIndex]
+        this.value = newValue;
+        this.setState({});
+    }
+
     render(){
-        return <td key={this.key}> {this.value} </td>
+        return <td key={this.key} onClick={this.onClick.bind(this)}> {this.value} </td>
     }
 }
--- src/config/typings.config.ts
+++ src/config/typings.config.ts
@@ -3,6 +3,7 @@ export const GRID_DEFAULT_HEIGHT = 15
 export interface ICell {
     key: number
     value: any
+    onClick?: (event: React.MouseEvent<HTMLTableDataCellElement>) => void
 }
 
 export interface IRow {
